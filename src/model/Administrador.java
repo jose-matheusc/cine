2cf5d6c7e96b0167275c189ae8cbbb3d368c8b94
Arthur53395 <arthur.leonardo@ufrpe.br>
2025-07-15 23:58:20 -0300

    Create Administrador.java

---
+++ src/model/Administrador.java
@@ -0,0 +1,10 @@
+package beans;
+
+public class Administrador extends Usuario {
+    public Administrador(String login, String senha) {
+        super(login, senha);
+    }
+
+    // Métodos específicos do administrador (a lógica será nos controllers)
+    // gerenciarFilmes(), gerenciarSalas(), configurarSessao(), etc.
+}
