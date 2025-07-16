d92618b72c3fda4e467084cc01b9a0a1425be142
Arthur53395 <arthur.leonardo@ufrpe.br>
2025-07-16 00:11:50 -0300

    Update Cliente.java

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -15,14 +15,37 @@ public class Cliente extends Usuario {
     }
 
     // Getters e Setters
-    public String getNome() { return nome; }
-    public void setNome(String nome) { this.nome = nome; }
-    public String getCpf() { return cpf; }
-    public void setCpf(String cpf) { this.cpf = cpf; }
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-    public String getTelefone() { return telefone; }
-    public void setTelefone(String telefone) { this.telefone = telefone; }
+    public String getNome() { 
+        return nome; 
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome; 
+    }
+    
+    public String getCpf() { 
+        return cpf; 
+    }
+    
+    public void setCpf(String cpf) { 
+        this.cpf = cpf; 
+    }
+    
+    public String getEmail() { 
+        return email; 
+    }
+    
+    public void setEmail(String email) { 
+        this.email = email; 
+    }
+    
+    public String getTelefone() { 
+        return telefone; 
+    }
+    
+    public void setTelefone(String telefone) { 
+        this.telefone = telefone; 
+    }
 
     @Override
     public String toString() {
