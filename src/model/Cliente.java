ac9fdd0ad5f5cd93488262abeff7aa6adf944fb8
Arthur53395 <arthur.leonardo@ufrpe.br>
2025-07-16 00:07:54 -0300

    Update Cliente.java

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -1,65 +1,37 @@
 package model;
 
-public class Cliente {
-    private Long id;
+public class Cliente extends Usuario {
     private String nome;
     private String cpf;
     private String email;
     private String telefone;
 
-    public Cliente(Long id, String nome, String cpf, String email, String telefone) {
-        this.id = id;
+    public Cliente(String login, String senha, String nome, String cpf, String email, String telefone) {
+        super(login, senha);
         this.nome = nome;
         this.cpf = cpf;
         this.email = email;
         this.telefone = telefone;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
+    // Getters e Setters
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    public String getCpf() { return cpf; }
+    public void setCpf(String cpf) { this.cpf = cpf; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getTelefone() { return telefone; }
+    public void setTelefone(String telefone) { this.telefone = telefone; }
 
     @Override
     public String toString() {
-        return String.format(
-                "Cliente{id=%d, nome='%s', cpf='%s', email='%s', telefone='%s'}",
-                id, nome, cpf, email, telefone
-        );
+        return "Cliente{" +
+               "nome='" + nome + '\'' +
+               ", cpf='" + cpf + '\'' +
+               ", email='" + email + '\'' +
+               ", telefone='" + telefone + '\'' +
+               ", login='" + login + '\'' +
+               '}';
     }
 }
