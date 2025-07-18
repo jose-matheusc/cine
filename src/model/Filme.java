39d4384b2e731cb03dbe8fb221b07878bc7cf2dd
Guilherme Guimarães <ninguinho21@gmail.com>
2025-07-18 17:51:13 -0300

    movie adjustments

--- src/model/Filme.java
+++ src/model/Filme.java
@@ -17,59 +17,20 @@ public class Filme {
         this.classificacaoIndicativa = classificacaoIndicativa;
     }
 
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getSinopse() {
-        return sinopse;
-    }
-
-    public void setSinopse(String sinopse) {
-        this.sinopse = sinopse;
-    }
-
-    public int getDuracao() {
-        return duracao;
-    }
-
-    public void setDuracao(int duracao) {
-        this.duracao = duracao;
-    }
-
-    public String getGenero() {
-        return genero;
-    }
-
-    public void setGenero(String genero) {
-        this.genero = genero;
-    }
-
-    public int getClassificacaoIndicativa() {
-        return classificacaoIndicativa;
-    }
-
-    public void setClassificacaoIndicativa(int classificacaoIndicativa) {
-        this.classificacaoIndicativa = classificacaoIndicativa;
-    }
+    public Long getId() { return id; }
+    public String getTitulo() { return titulo; }
+    public void setTitulo(String titulo) { this.titulo = titulo; }
+    public String getSinopse() { return sinopse; }
+    public void setSinopse(String sinopse) { this.sinopse = sinopse; }
+    public int getDuracao() { return duracao; }
+    public void setDuracao(int duracao) { this.duracao = duracao; }
+    public String getGenero() { return genero; }
+    public void setGenero(String genero) { this.genero = genero; }
+    public int getClassificacaoIndicativa() { return classificacaoIndicativa; }
+    public void setClassificacaoIndicativa(int classificacaoIndicativa) { this.classificacaoIndicativa = classificacaoIndicativa; }
 
     @Override
     public String toString() {
-        return "Filme [" +
-                "ID=" + id +
-                ", Título='" + titulo + '\'' +
-                ", Duração=" + duracao + " min" +
-                ", Gênero='" + genero + '\'' +
-                ", Classificação=" + classificacaoIndicativa + " anos" +
-                ']';
+        return "Filme [ID=" + id + ", Título='" + titulo + "']";
     }
 }
